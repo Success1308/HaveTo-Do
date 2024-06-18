39c0586e6cce423d1345d17e30327d2f78ae161c
success1308 <success.ambaliya13@gmail.com>
2024-06-18 12:14:44 -0400

    Todo

--- src/projectDOM.js
+++ src/projectDOM.js
@@ -1,225 +1,222 @@
 // projectDOm.js
 
-import Storage from './storage';
-import Project from './project';
-import {renderTodos} from './todoDom';
-
+import Storage from "./storage";
+import Project from "./project";
+import { renderTodos } from "./todoDom";
 
 const storage = Storage();
 
 let currentProject = "Today";
 
 export function setCurrent(projectName) {
-    currentProject = projectName;
-    return currentProject;
+  currentProject = projectName;
+  return currentProject;
 }
 
 export function getCurrent() {
-    return currentProject;
+  return currentProject;
 }
 
 // Create project-list div
-const projectList = document.createElement('div');
-projectList.className = 'project-list';
+const projectList = document.createElement("div");
+projectList.className = "project-list";
 
 // Create default project div
-const projectDefault = document.createElement('div');
-export {projectDefault};
+const projectDefault = document.createElement("div");
+export { projectDefault };
 
 // create project
 function addProject(projectName) {
-    const projectCre = Project(projectName);
-    storage.saveProject(projectCre);
-    const projectElement = createProjectElement(projectCre);
-    projectList.appendChild(projectElement);
+  const projectCre = Project(projectName);
+  storage.saveProject(projectCre);
+  const projectElement = createProjectElement(projectCre);
+  projectList.appendChild(projectElement);
 }
 
 // append project
 function createProjectElement(project) {
-    const projectElement = document.createElement('div');
-    projectElement.className = 'project';
+  const projectElement = document.createElement("div");
+  projectElement.className = "project";
 
-    const projectContent = document.createElement('p');
-    projectContent.className = 'project-content';
-    projectContent.textContent = project.projectName;
+  const projectContent = document.createElement("p");
+  projectContent.className = "project-content";
+  projectContent.textContent = project.projectName;
 
-    const removeButton = document.createElement('button');
-    removeButton.className = 'remove-project';
-    removeButton.innerHTML = '<i class="fas fa-times"></i>';
-    removeButton.addEventListener('click', () => {
-        setCurrent(project.projectName);
-        removeProject(project.projectName);     
-        
+  const removeButton = document.createElement("button");
+  removeButton.className = "remove-project";
+  removeButton.innerHTML = '<i class="fas fa-times"></i>';
+  removeButton.addEventListener("click", () => {
+    setCurrent(project.projectName);
+    removeProject(project.projectName);
+  });
+
+  projectElement.appendChild(projectContent);
+  projectElement.appendChild(removeButton);
+
+  projectElement.addEventListener("click", () => {
+    setCurrent(project.projectName);
+
+    const other1 = document.querySelectorAll(".project");
+    other1.forEach((element) => {
+      element.style.backgroundColor = "#fbf8ff";
     });
+    projectElement.style.backgroundColor = "#e4d9f2";
+    projectDefault.style.backgroundColor = "#fbf8ff";
 
-    projectElement.appendChild(projectContent);
-    projectElement.appendChild(removeButton);
-
-    projectElement.addEventListener('click', () => {
-        setCurrent(project.projectName);
-
-        const other1 = document.querySelectorAll('.project');  
-        other1.forEach(element => {
-            element.style.backgroundColor = "#fbf8ff";
-        });
-        projectElement.style.backgroundColor = "#e4d9f2";
-        projectDefault.style.backgroundColor = "#fbf8ff";  
-        
-        
-        renderTodos(getCurrent());
-    });
-    return projectElement;
+    renderTodos(getCurrent());
+  });
+  return projectElement;
 }
-
 
 // remove projects
 function removeProject(projectName) {
-    // Remove from UI
-    const projectElements = document.querySelectorAll('.project');
-    projectElements.forEach(element => {
-        if (element.textContent.trim() === projectName) {
-            element.remove();            
-        }
-        
-    });
-    // Remove from storage
-    storage.removeProject(projectName);
+  // Remove from UI
+  const projectElements = document.querySelectorAll(".project");
+  projectElements.forEach((element) => {
+    if (element.textContent.trim() === projectName) {
+      element.remove();
+    }
+  });
+  // Remove from storage
+  storage.removeProject(projectName);
 }
 
 export function createLeftSide() {
-    
-    // Create leftSide div
-    const leftSide = document.createElement('div');
-    leftSide.className = 'leftSide';
+  // Create leftSide div
+  const leftSide = document.createElement("div");
+  leftSide.className = "leftSide";
 
-    // Create h4 element for Projects
-    const h4 = document.createElement('h4');
-    h4.textContent = 'Projects';
+  // Create h4 element for Projects
+  const h4 = document.createElement("h4");
+  h4.textContent = "Projects";
 
-    projectList.innerHTML = '';
-    projectDefault.className = 'project-default';
-    projectDefault.style.backgroundColor = "#e4d9f2"; 
-    
-    const projectDefaultContent = document.createElement('p');
-    projectDefaultContent.className = 'project-content';
-    projectDefaultContent.textContent = 'Today';
+  projectList.innerHTML = "";
+  projectDefault.className = "project-default";
+  projectDefault.style.backgroundColor = "#e4d9f2";
 
-    let todayProject = storage.getProject("Today");
-    if (!todayProject) {
-        const projectDef = Project("Today");
-        setCurrent(projectDef.projectName);
-        storage.saveProject(projectDef);
-        projectDefault.appendChild(projectDefaultContent); 
-         
-        projectDefault.addEventListener('click', () => {
-    
-            setCurrent(projectDef.projectName);
-    
-            projectDefault.style.backgroundColor = "#e4d9f2"; 
-            const other = document.querySelectorAll('.project');  
-            other.forEach(element => {
-                element.style.backgroundColor = "#fbf8ff";
-            }); 
-    
-            renderTodos(getCurrent());
-        });
-        projectList.appendChild(projectDefault);
+  const projectDefaultContent = document.createElement("p");
+  projectDefaultContent.className = "project-content";
+  projectDefaultContent.textContent = "Today";
 
-    }else{
-        setCurrent(todayProject.projectName);
-        storage.saveProject(todayProject);
-        projectDefault.appendChild(projectDefaultContent); 
-        projectList.appendChild(projectDefault);
-        projectDefault.addEventListener('click', () => {
-    
-            setCurrent(todayProject.projectName);
-    
-            projectDefault.style.backgroundColor = "#e4d9f2"; 
-            const other = document.querySelectorAll('.project');  
-            other.forEach(element => {
-                element.style.backgroundColor = "#fbf8ff";
-            }); 
-    
-            renderTodos(getCurrent());
-        });
-    }
-    
+  let todayProject = storage.getProject("Today");
+  if (!todayProject) {
+    const projectDef = Project("Today");
+    setCurrent(projectDef.projectName);
+    storage.saveProject(projectDef);
+    projectDefault.appendChild(projectDefaultContent);
 
-    // Create input-left div
-    const inputLeft = document.createElement('div');
-    inputLeft.className = 'input-left';
+    projectDefault.addEventListener("click", () => {
+      setCurrent(projectDef.projectName);
 
-    const addIcon = document.createElement('i');
-    addIcon.className = 'fa-solid fa-square-plus project-add-icon';
-    addIcon.id = 'addProjectButton';
+      projectDefault.style.backgroundColor = "#e4d9f2";
+      const other = document.querySelectorAll(".project");
+      other.forEach((element) => {
+        element.style.backgroundColor = "#fbf8ff";
+      });
 
-    const inputContainer = document.createElement('div');
-    inputContainer.className = 'input-container-p';
-    inputContainer.style.display = 'none';
+      renderTodos(getCurrent());
+    });
+    projectList.appendChild(projectDefault);
+  } else {
+    setCurrent(todayProject.projectName);
+    storage.saveProject(todayProject);
+    projectDefault.appendChild(projectDefaultContent);
+    projectList.appendChild(projectDefault);
+    projectDefault.addEventListener("click", () => {
+      setCurrent(todayProject.projectName);
 
-    const textarea = document.createElement('textarea');
-    textarea.id = 'project-input';
-    textarea.placeholder = 'Enter project name';
-    const addButton = document.createElement('button');
-    addButton.id = 'add-project-btn';
-    addButton.textContent = 'Add';
+      projectDefault.style.backgroundColor = "#e4d9f2";
+      const other = document.querySelectorAll(".project");
+      other.forEach((element) => {
+        element.style.backgroundColor = "#fbf8ff";
+      });
 
-    addIcon.addEventListener('click', () => {
-        inputContainer.style.display = inputContainer.style.display === 'block' ? 'none' : 'block';
-        addIcon.className = inputContainer.style.display === 'block' ? 'fa-solid fa-xmark project-add-icon' : 'fa-solid fa-square-plus project-add-icon';
+      renderTodos(getCurrent());
+    });
+  }
 
-        addButton.addEventListener('click', () => {
-            const projectName = textarea.value.trim();
-            if (projectName) {
-                addProject(projectName);
-                textarea.value = '';
-                inputContainer.style.display = 'none';
-                addIcon.className = inputContainer.style.display === 'block' ? 'fa-solid fa-xmark project-add-icon' : 'fa-solid fa-square-plus project-add-icon';
-            }
-        });
+  // Create input-left div
+  const inputLeft = document.createElement("div");
+  inputLeft.className = "input-left";
 
-        textarea.addEventListener('keypress', (event) => {
-            if (event.key === 'Enter') {
-              const projectName = textarea.value.trim();
-              if (projectName) {
-                addProject(projectName);
-                textarea.value = '';
-                inputContainer.style.display = 'none';
-                addIcon.className = inputContainer.style.display === 'block' ? 'fa-solid fa-xmark project-add-icon' : 'fa-solid fa-square-plus project-add-icon';
-              }
-            }
-        });
+  const addIcon = document.createElement("i");
+  addIcon.className = "fa-solid fa-square-plus project-add-icon";
+  addIcon.id = "addProjectButton";
 
-        document.addEventListener('click', (event) => {
-            if (!inputContainer.contains(event.target) && event.target !== addIcon) {
-              inputContainer.style.display = 'none';
-              addIcon.className = 'fa-solid fa-square-plus project-add-icon';
-            }
-          });
+  const inputContainer = document.createElement("div");
+  inputContainer.className = "input-container-p";
+  inputContainer.style.display = "none";
+
+  const textarea = document.createElement("textarea");
+  textarea.id = "project-input";
+  textarea.placeholder = "Enter project name";
+  const addButton = document.createElement("button");
+  addButton.id = "add-project-btn";
+  addButton.textContent = "Add";
+
+  addIcon.addEventListener("click", () => {
+    inputContainer.style.display =
+      inputContainer.style.display === "block" ? "none" : "block";
+    addIcon.className =
+      inputContainer.style.display === "block"
+        ? "fa-solid fa-xmark project-add-icon"
+        : "fa-solid fa-square-plus project-add-icon";
+
+    addButton.addEventListener("click", () => {
+      const projectName = textarea.value.trim();
+      if (projectName) {
+        addProject(projectName);
+        textarea.value = "";
+        inputContainer.style.display = "none";
+        addIcon.className =
+          inputContainer.style.display === "block"
+            ? "fa-solid fa-xmark project-add-icon"
+            : "fa-solid fa-square-plus project-add-icon";
+      }
     });
 
-    inputContainer.appendChild(textarea);
-    inputContainer.appendChild(addButton);
-    inputLeft.appendChild(addIcon);
-    inputLeft.appendChild(inputContainer);
-    // Append elements to leftSide div
-    leftSide.appendChild(h4);
-    leftSide.appendChild(projectList);
-    leftSide.appendChild(inputLeft);
-
-    let allProjects = storage.loadProjects();
-
-    allProjects.forEach(element => {
-        if (element.projectName !== "Today") {
-            const projectElement1 = createProjectElement(element);
-            projectList.appendChild(projectElement1);
+    textarea.addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        const projectName = textarea.value.trim();
+        if (projectName) {
+          addProject(projectName);
+          textarea.value = "";
+          inputContainer.style.display = "none";
+          addIcon.className =
+            inputContainer.style.display === "block"
+              ? "fa-solid fa-xmark project-add-icon"
+              : "fa-solid fa-square-plus project-add-icon";
         }
+      }
     });
 
-    renderTodos(getCurrent());
+    document.addEventListener("click", (event) => {
+      if (!inputContainer.contains(event.target) && event.target !== addIcon) {
+        inputContainer.style.display = "none";
+        addIcon.className = "fa-solid fa-square-plus project-add-icon";
+      }
+    });
+  });
 
-    return leftSide;
+  inputContainer.appendChild(textarea);
+  inputContainer.appendChild(addButton);
+  inputLeft.appendChild(addIcon);
+  inputLeft.appendChild(inputContainer);
+  // Append elements to leftSide div
+  leftSide.appendChild(h4);
+  leftSide.appendChild(projectList);
+  leftSide.appendChild(inputLeft);
+
+  let allProjects = storage.loadProjects();
+
+  allProjects.forEach((element) => {
+    if (element.projectName !== "Today") {
+      const projectElement1 = createProjectElement(element);
+      projectList.appendChild(projectElement1);
+    }
+  });
+
+  renderTodos(getCurrent());
+
+  return leftSide;
 }
-
-
-
