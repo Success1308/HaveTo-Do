1ec2f21f54eb631c72b470a83c8812331b5844b7
success1308 <success.ambaliya13@gmail.com>
2024-06-24 14:09:28 -0400

    Weather

--- src/storage.js
+++ src/storage.js
@@ -23,9 +23,8 @@ export default function Storage() {
     );
     return gotProject;
   };
-
+  const projects = [];
   const loadProjects = () => {
-    let projects = [];
     for (let i = 0; i < localStorage.length; i++) {
       const key = localStorage.key(i);
       if (key.startsWith("project-")) {
